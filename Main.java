e73f820730dbed7bf7d20da6bd28dcfd93f5c9a7
Ethan <Ethan.lynn2027@gbwl.org>
2026-03-10 14:38:54 +0000

    4.12 Notes

--- Main.java
+++ Main.java
@@ -48,6 +48,42 @@ public class Main {
    System.out.println( bingo[0][0] );
    System.out.println( bingo[1][1] ); // heart
    System.out.println( bingo[1][2] ); // smiley
+
+   // 4.12 2D ARRAY TRAVERSALS (loops)
+   // A ROW in a 2D array is just a 1D array
+   // when we loop, we go ROW first
+   // then every COL in that row
+
+   // ENHANCED FOR-EACH loops (nested)
+   // outer loop iterates through all ROWS
+   for ( String[] row : seatingChart ) {
+      // for every col in that current row
+      for ( String col : row) {
+         System.out.print(col + "");
+      }
+      System.out.println();
+   }
+   // remember that FOR-EACH loops arre only
+   // for visiting every time, not changing values
+
+   // STANDARD for loops require tracking index
+   // and knowing how many items in each row/col
+   // array.length provides # of ROWS in a 2D array
+   int numRows = seatingChart.length; // 3
+   // array [0].length provides # of COLS
+   int numCols = seatingChart[0].length;
+   // seatingChart[0] is literally the whole first row
+
+   // use this info in the standard for loop
+   for (int r = 0; r < seatingChart.length; r++)
+      for (int c = 0; c < seatingChart[0].length; c++) {
+         // can modify values in a standard for loop!
+         seatingChart[r][c] = seatingChart[r][c] + "🪑";
+         System.out.print( seatingChart[r][c] + " ");
+
+    }
+    System.out.println(); // new line every row
+
    
    }
 }
